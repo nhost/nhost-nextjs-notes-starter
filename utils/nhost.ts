dbac545b2ff97c1c25cc57cb430c7860e542819a
het1194 <124895020+het1194@users.noreply.github.com>
2023-02-09 21:22:18 +0530

    Update nhost.ts

--- utils/nhost.ts
+++ utils/nhost.ts
@@ -2,8 +2,8 @@ import { NhostClient } from "@nhost/nextjs";
 
 // TODO: Update subdomain and region to your own project.
 const nhost = new NhostClient({
-  subdomain: "absvxxnsesanljntvxui",
-  region: "eu-central-1",
+  subdomain: "vqxvucihdglxvqyxlhir",
+  region: "ap-south-1",
 });
 
 export { nhost };
